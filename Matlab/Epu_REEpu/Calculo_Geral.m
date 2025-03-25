c92b5d44f557cb0027c131b2a57bb2b6715e86e5
gabbil <gabbil181@gmail.com>
2025-03-25 19:15:24 -0300

    Matlab

---
+++ Matlab/Epu_REEpu/Calculo_Geral.m
@@ -0,0 +1,26 @@
+clear all
+clc
+
+EEEW_Alimentador_DFIG_AT
+EEEW_Alimentador_DFIG_BT
+EEEW_Alimentador_DFIG_CT
+EEEW_Alimentador_DFIG_ABT
+EEEW_Alimentador_DFIG_BCT
+EEEW_Alimentador_DFIG_CAT
+EEEW_Alimentador_DFIG_AB
+EEEW_Alimentador_DFIG_BC
+EEEW_Alimentador_DFIG_CA
+EEEW_Alimentador_DFIG_ABC
+
+EEEW_Alimentador_PMSG_AT
+EEEW_Alimentador_PMSG_BT
+EEEW_Alimentador_PMSG_CT
+EEEW_Alimentador_PMSG_ABT
+EEEW_Alimentador_PMSG_BCT
+EEEW_Alimentador_PMSG_CAT
+EEEW_Alimentador_PMSG_AB
+EEEW_Alimentador_PMSG_BC
+EEEW_Alimentador_PMSG_CA
+EEEW_Alimentador_PMSG_ABC
+
+disp('Cálculo Finalizado com Éxito')
