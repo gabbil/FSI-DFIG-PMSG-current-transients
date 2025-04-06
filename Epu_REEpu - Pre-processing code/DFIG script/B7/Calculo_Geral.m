68484f5b94ad9b32973d585e2950409ec33ddaeb
GabrielaPerez <gabbil181@gmail.com>
2025-04-06 13:46:17 -0300

    Add files via upload

---
+++ Epu_REEpu - Pre-processing code/DFIG script/B7/Calculo_Geral.m
@@ -0,0 +1,15 @@
+clear all
+clc
+
+EEEW_Ramal_7_DFIG_AT
+EEEW_Ramal_7_DFIG_BT
+EEEW_Ramal_7_DFIG_CT
+EEEW_Ramal_7_DFIG_ABT
+EEEW_Ramal_7_DFIG_BCT
+EEEW_Ramal_7_DFIG_CAT
+EEEW_Ramal_7_DFIG_AB
+EEEW_Ramal_7_DFIG_BC
+EEEW_Ramal_7_DFIG_CA
+EEEW_Ramal_7_DFIG_ABC
+
+disp('Cálculo Finalizado com Éxito')
---
+++ Epu_REEpu - Pre-processing code/DFIG script/MF/Calculo_Geral.m
@@ -0,0 +1,15 @@
+clear all
+clc
+
+EEEW_Alimentador_DFIG_AT
+EEEW_Alimentador_DFIG_BT
+EEEW_Alimentador_DFIG_CT
+EEEW_Alimentador_DFIG_ABT
+EEEW_Alimentador_DFIG_BCT
+EEEW_Alimentador_DFIG_CAT
+EEEW_Alimentador_DFIG_AB
+EEEW_Alimentador_DFIG_BC
+EEEW_Alimentador_DFIG_CA
+EEEW_Alimentador_DFIG_ABC
+
+disp('Cálculo Finalizado com Éxito')
